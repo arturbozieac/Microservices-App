d9ea91393ad9a5ce046b12faa55e425254046fff
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2023-06-05 00:47:53 +0300

    #4 add getStatistics endpoint & TC

--- challenges-server/src/main/java/app/controllers/ChallengeAttemptController.java
+++ challenges-server/src/main/java/app/controllers/ChallengeAttemptController.java
@@ -1,12 +1,16 @@
 package app.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.model.challenge.ChallengeAttempt;
@@ -27,4 +31,9 @@ public class ChallengeAttemptController {
 	ResponseEntity<ChallengeAttempt> postResult(@RequestBody @Valid ChallengeAttemptDTO challengeAttemptDTO) {
 		return ResponseEntity.ok(challengeService.verifyAttempt(challengeAttemptDTO));
 	}
+	
+  @GetMapping
+  ResponseEntity<List<ChallengeAttempt>> getStatistics(@RequestParam("alias") String alias) {
+    return ResponseEntity.ok(challengeService.getStatsForUser(alias));
+  }
 }
--- challenges-server/src/test/java/app/controllers/ChallengeAttemptControllerTest.java
+++ challenges-server/src/test/java/app/controllers/ChallengeAttemptControllerTest.java
@@ -4,8 +4,11 @@ import static org.assertj.core.api.BDDAssertions.then;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +17,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -43,10 +48,14 @@ class ChallengeAttemptControllerTest {
 	@Autowired
 	private MockMvc mvc;
 
-	@Autowired
-	private JacksonTester<ChallengeAttemptDTO> jsonRequestAttempt;
-	@Autowired
-	private JacksonTester<ChallengeAttempt> jsonResultAttempt;
+  @Autowired
+  private JacksonTester<ChallengeAttemptDTO> jsonRequestAttempt;
+
+  @Autowired
+  private JacksonTester<ChallengeAttempt> jsonResultAttempt;
+
+  @Autowired
+  private JacksonTester<List<ChallengeAttempt>> jsonResultAttemptList;
 
 	/**
 	 * Test valid challenge attempt.
@@ -59,7 +68,7 @@ class ChallengeAttemptControllerTest {
 		User user = new User(1L, "john");
 		long attemptId = 5L;
 		ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 70, "john", 3500);
-		ChallengeAttempt expectedResponse = new ChallengeAttempt(attemptId, user, 50, 70, 3500, true);
+		ChallengeAttempt expectedResponse = new ChallengeAttempt(attemptId, user, attemptDTO.getFactorA(), attemptDTO.getFactorB(), 3500, true);
 		given(challengeService.verifyAttempt(eq(attemptDTO))).willReturn(expectedResponse);
 		// when
 		MvcResult response = mvc.perform(post("/attempts").contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -81,4 +90,31 @@ class ChallengeAttemptControllerTest {
 		mvc.perform(post("/attempts").contentType(MediaType.APPLICATION_JSON_VALUE)
 				.content(jsonRequestAttempt.write(attemptDTO).getJson())).andExpect(status().isBadRequest()).andReturn();
 	}
+	
+	/**
+   * Test valid challenge attempt.
+   * 
+   * @throws Exception
+   */
+	@Test
+  public void getUserStats() throws Exception {
+      // given
+      User user = new User("john_doe");
+      ChallengeAttempt attempt1 = new ChallengeAttempt(1L, user, 50, 70, 3500, true);
+      ChallengeAttempt attempt2 = new ChallengeAttempt(2L, user, 20, 10, 210, false);
+      List<ChallengeAttempt> recentAttempts = List.of(attempt1, attempt2);
+      given(challengeService
+              .getStatsForUser("john_doe"))
+              .willReturn(recentAttempts);
+
+      // when
+      MvcResult response = mvc.perform(get("/attempts").contentType(MediaType.APPLICATION_JSON_VALUE).param("alias", "john_doe"))
+          .andExpect(status().isOk()).andReturn();
+
+      // then
+      then(response.getResponse().getContentAsString()).isEqualTo(
+              jsonResultAttemptList.write(
+                      recentAttempts
+              ).getJson());
+  }
 }
